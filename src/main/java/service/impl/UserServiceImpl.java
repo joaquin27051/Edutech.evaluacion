2bba7f9a31ec1b056a38f9d96cab7cbf0b66c906
joaquin <joaq.pastenes@duocuc.cl>
2025-05-19 11:59:28 -0400

    codigo impl
    Este código nos ayudara ha ser mas escalable  y tener una organización clara al momento de seguir con el proyecto

---
+++ src/main/java/service/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package service.impl;
+
+import Model.User;
+import Repository.UserRepository;
+import service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow();
+    }
+}
